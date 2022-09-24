4946723be2dde0344bd06de02da7a7e4b5ba8f85
G:\Git <ernesto.zanabria1979@gmail.com>
2022-09-24 18:55:59 -0400

    Modified project1/os_script.js
    deleted the animation jquery code

--- project1/os_script.js
+++ project1/os_script.js
@@ -1,32 +1,3 @@
-//animation.
-function animation(){
-    $(document).ready(function () {
-        $('.dashboard1, .dashboard2, .dashboard3, .dashboard4').hover(function () {
-                // in
-                $(this).stop().animate(
-                    {
-                        left: '0',
-                        backgroundColor: 'rgba(207, 207, 199, 0.3)'
-                    },
-                    500,
-                    'easeInSine'
-                );// end animate.
-            }, 
-            function () {
-                // out
-                $(this).stop().animate(
-                    {
-                        left: '-250px',
-                        backgroundColor: 'rgb(151, 146, 139)'
-                    },
-                    1500,
-                    'easeOutBounce'
-                );// end animate.
-            }
-        );// end hover.
-    });// end function.
-}
-
 //the quiz.
 function quiz(){
     var score = 0;
