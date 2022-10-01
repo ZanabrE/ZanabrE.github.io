de4fe9a4f04ce62b6cf4ec26c381a0c83b45fea4
G:\Git <ernesto.zanabria1979@gmail.com>
2022-09-30 22:31:37 -0400

    Modified project2/gd_script.js
    no comments

--- project2/gd_script.js
+++ project2/gd_script.js
@@ -1,6 +1,54 @@
 //This is a jQuery file.
 function show(){
     $(document).ready(function(){
-        $('.uno').hide();
+        $('h2, .text1').hide();
+        //start mouse over.
+        $('img').hover(function () {
+                // over
+                $('h2, .text1').show(3000);
+            }, function () {
+                // out
+                $('h2, .text1').hide();
+            }
+        );
+    });
+
+    $(document).ready(function(){
+        $('.duo, .text2').hide();
+        //start mouse over.
+        $('.image2').hover(function () {
+                // over
+                $('.duo, .text2').show(3000);
+            }, function () {
+                // out
+                $('.duo, .text2').hide();
+            }
+        );
+    });
+
+    $(document).ready(function(){
+        $('.tre, .text3').hide();
+        //start mouse over.
+        $('.image3').hover(function () {
+                // over
+                $('.tre, .text3').show(3000);
+            }, function () {
+                // out
+                $('.tre, .text3').hide();
+            }
+        );
+    });
+
+    $(document).ready(function(){
+        $('.quattro, .text4').hide();
+        //start mouse over.
+        $('.image4').hover(function () {
+                // over
+                $('.quattro, .text4').show(3000);
+            }, function () {
+                // out
+                $('.quattro, .text4').hide();
+            }
+        );
     });
 }
