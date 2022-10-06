e3be946ab76ada13ae6310aea16a96b7e10660bd
G:\Git <ernesto.zanabria1979@gmail.com>
2022-10-05 21:52:54 -0400

    Modified project3/cmd_script.js

--- project3/cmd_script.js
+++ project3/cmd_script.js
@@ -17,47 +17,53 @@ function images(){
 //first javascript function.
 function answer1(){
     //variables.
-    /*var imagen = document.getElementById("ipconfig");*/
-    let x = document.getElementById("command-1").value;
-    let command = 'ipconfig /all';
-    let text;
-
-    //conditional statements.
-    if(x == command){
-        text = "Correct!<br>"
-              +"On the image below you IP address is found on IPv4<br>"
-              +" and your MAC address is the Physical Address.";
-    }
-    else{
-        text = "Please enter the command.";
-    }
-    document.getElementById("message1").innerHTML = text;
-    /*imagen.show(2000);*/
-
-    //Added jquery inside my function to show the image.
-    $(document).ready(function () {
-        if(x == command){
-            $('#ipconfig').show(2000);
-        }
-        if(x != command){
-            $('#ipconfig').hide(2000);
-        }
-    });
+    let x = document.getElementById("command1").value;
+	let command = 'ipconfig /all';
+	let text;
+	
+	if(x == command){
+		text = "Correct!<br>" 
+			  +"On the image below your IP address is found on IPv4<br>"
+		      +" and your MAC address is the Physical Address.";
+	}else{
+		text = "Please enter the command.";
+	}
+	document.getElementById("message1").innerHTML = text;
+	
+	//Added jQuery inside my function to show the image.
+	$("document").ready(function(){
+		if(x == command){
+			$('#ipconfig').show(3000);
+		}
+		if(x != command){
+			$('#ipconfig').hide();
+		}
+	});
 }
 
 //second javascript function.
 function answer2(){
     //Variables.
     let x = document.getElementById("command2").value;
-    let command = 'ifconfig -l';
-    let text;
-
-    //Conditional statement.
-    if(x == command){
-        text = "Correct!<br>"
-    }
-    else{
-        text = "Please enter the command.";
-    }
-    document.getElementById("message2").innerHTML = text;
+	let command = 'ifconfig -l';
+	let text;
+	
+	if(x == command){
+		text = "Correct!<br>"
+			  +"On the image below your IP address is inet<br>"
+		      +" and your MAC address is ether.";
+	}else{
+		text = "Please enter the command.";
+	}
+	document.getElementById("message2").innerHTML = text;
+	
+	//Added jQuery inside my function to show the image.
+	$("document").ready(function(){
+		if(x == command){
+			$('#ifconfig').show(3000);
+		}
+		if(x != command){
+			$('#ifconfig').hide();
+		}
+	});
 }
